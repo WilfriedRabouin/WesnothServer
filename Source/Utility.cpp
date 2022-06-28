6184774ac3e1091f6bf67273a574c757e9793b25
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2022-06-28 21:35:32 +0100

    Extract compress utility functions

---
+++ Source/Utility.cpp
@@ -0,0 +1,51 @@
+/*
+Copyright (C) 2022 Wilfried Rabouin
+
+This file is part of WesnothServer.
+
+WesnothServer is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+WesnothServer is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with WesnothServer.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <sstream>
+
+#include <boost/iostreams/filtering_streambuf.hpp>
+#include <boost/iostreams/filter/gzip.hpp>
+#include <boost/iostreams/copy.hpp>
+
+#include "Utility.hpp"
+
+namespace Utility
+{
+	[[nodiscard]] std::string Compress(const char* source, std::size_t sourceSize)
+	{
+		boost::iostreams::filtering_istreambuf buffer{};
+		buffer.push(boost::iostreams::gzip_compressor());
+		buffer.push(boost::iostreams::array_source{ source, sourceSize });
+
+		std::stringstream stringStream{};
+		boost::iostreams::copy(buffer, stringStream);
+		return stringStream.str();
+	}
+
+	[[nodiscard]] std::string Uncompress(const char* source, std::size_t sourceSize)
+	{
+		boost::iostreams::filtering_istreambuf buffer{};
+		buffer.push(boost::iostreams::gzip_decompressor());
+		buffer.push(boost::iostreams::array_source{ source, sourceSize });
+
+		std::stringstream stringStream{};
+		boost::iostreams::copy(buffer, stringStream);
+		return stringStream.str();
+	}
+}
