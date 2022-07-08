287741d6849b636fe79662097fa99ed304be2e1e
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2022-07-08 20:51:54 +0100

    increase max number of clients to 8 for multiplayer purpose

--- Source/Config.hpp
+++ Source/Config.hpp
@@ -40,7 +40,7 @@ public:
 	Config& operator=(const Config&) = delete;
 	Config& operator=(Config&&) = delete;
 
-	std::size_t clientCountLimit{ 4 };
+	std::size_t clientCountLimit{ 8 };
 	std::size_t bufferCapacity{ 128 };
 	bool isThreadCountAuto{ true };
 	std::size_t threadCount{ 1 };
